9da980ed9417596473b4b04c4c396714cd12f86c
yo1995 <cht@bupt.edu.cn>
2018-08-17 11:44:53 +0800

    added pangps gui

---
+++ Tool_PanGPSloader/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //开启服务   
+            ProcessStartInfo a = new ProcessStartInfo(@"c:/windows/system32/cmd.exe", "/c  net start PanGPS");
+            a.WindowStyle = ProcessWindowStyle.Hidden;
+            Process process = Process.Start(a);
+            Process.Start(@"c:/ProgramData/Microsoft/Windows/Start Menu/Programs/Palo Alto Networks/GlobalProtect/GlobalProtect.lnk");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo a = new ProcessStartInfo(@"c:/windows/system32/cmd.exe", "/c  net stop PanGPS");
+            a.WindowStyle = ProcessWindowStyle.Hidden;
+            Process process = Process.Start(a);
+        }
+    }
+}
